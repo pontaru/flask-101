b1df4c29ca812e6f0feedf542d0446f6f3c0b033
Lizet Hernandez Vargas <rizeto@gmail.com>
2022-06-09 16:14:39 +0200

    up

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,14 @@
 from flask import Flask
+import jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello World!"
+@app.route('/api/v1/products')
+def return_json():
+    return jsonify({'PRODUCTS' : {
+    1: { 'id': 1, 'name': 'Skello' },
+    2: { 'id': 2, 'name': 'Socialive.tv' },
+}
+})
